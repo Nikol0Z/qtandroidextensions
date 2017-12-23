c4eee8f9090164c25282e0b7ee20747795dcf87a
Uladzislau Vasilyeu <vasvlad@gmail.com>
2017-12-23 09:36:41 +0300

    Began adapted to WSO2

---
+++ QtAndroidHelpers/Mobility/QAndroidWso2GPSLocationDataProvider.cpp
@@ -0,0 +1,97 @@
+/*
+	Offscreen Android Views library for Qt
+
+    Authors:
+    Uladzislau Vasilyeu <vasvlad@gmail.com>
+
+    Distrbuted under The BSD License
+
+    Copyright (c) 2017.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	* Redistributions of source code must retain the above copyright notice,
+	  this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+	* Neither the name of the DoubleGIS, LLC nor the names of its contributors
+	  may be used to endorse or promote products derived from this software
+	  without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+	THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+	BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+	THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "QAndroidWso2GPSLocationDataProvider.h"
+#include <QAndroidQPAPluginGap.h>
+#include "TJniObjectLinker.h"
+
+
+namespace Mobility {
+
+
+Q_DECL_EXPORT void JNICALL Java_ScannerListener_scannerInfoUpdate(JNIEnv *, jobject, jlong native_ptr, jboolean code)
+{
+	JNI_LINKER_OBJECT(Mobility::QAndroidWso2GPSLocationDataProvider, native_ptr, proxy)
+	proxy->scannerInfo(code);
+}
+
+
+static const JNINativeMethod methods[] = {
+	{"getContext", "()Landroid/content/Context;", (void*)QAndroidQPAPluginGap::getCurrentContext},
+	{"scannerInfoUpdate", "(JZ)V", (void*)Java_ScannerListener_scannerInfoUpdate},
+};
+
+
+JNI_LINKER_IMPL(QAndroidWso2GPSLocationDataProvider, "ru/dublgis/androidhelpers/mobility/Wso2GPSLocationListiner", methods)
+
+QAndroidWso2GPSLocationDataProvider::QAndroidWso2GPSLocationDataProvider(QObject * parent)
+	: QObject(parent)
+	, jniLinker_(new JniObjectLinker(this))
+{
+}
+
+
+QAndroidWso2GPSLocationDataProvider::~QAndroidWso2GPSLocationDataProvider()
+{
+}
+
+
+bool QAndroidWso2GPSLocationDataProvider::init()
+{
+    bool result = false;
+	if (isJniReady())
+	{
+		result = jni()->callBool("init");
+	}
+    return result;
+}
+
+QString QAndroidWso2GPSLocationDataProvider::result()
+{
+    QString result;
+	if (isJniReady())
+	{
+		result = jni()->callString("result");
+	}
+    return result;
+}
+
+
+void QAndroidWso2GPSLocationDataProvider::scannerInfo(bool code)
+{
+    emit scannerInfoUpdate(code);
+}
+
+}
---
+++ QtAndroidHelpers/ru.dublgis.androidhelpers/mobility/Wso2GPSLocationListiner.java
@@ -0,0 +1,120 @@
+/*
+    Offscreen Android Views library for Qt
+
+    Authors:
+    Uladzislau Vasilyeu <vasvlad@gmail.com>
+
+    Distrbuted under The BSD License
+
+    Copyright (c) 2017.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of the DoubleGIS, LLC nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+    THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package ru.dublgis.androidhelpers.mobility;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import ru.dublgis.androidhelpers.Log;
+import android.os.RemoteException;
+import device.common.DecodeResult;
+import device.common.ScanConst;
+import device.sdk.ScanManager;
+
+
+public class Wso2GPSLocationListiner extends BroadcastReceiver
+{
+    final private static String LOG_TAG = "Grym/Wso2GPSLocationListiner";
+    final private static boolean verbose_ = false;
+    private static long native_ptr_ = 0;
+    private boolean started_ = false;
+
+	private static ScanManager iScanner;
+	private static DecodeResult mDecodeResult;
+    private static String result = "";
+
+    public Wso2GPSLocationListiner(){
+    }
+
+    public Wso2GPSLocationListiner(long native_ptr)
+    {
+        native_ptr_ = native_ptr;
+    }
+
+
+    String result(){
+        synchronized(result){
+            return result;
+        }
+    }
+
+    public void onReceive(Context context, Intent intent) {
+        Log.e(LOG_TAG, "Broadcast ");
+        if (iScanner != null) {
+            iScanner.aDecodeGetResult(mDecodeResult.recycle());
+            Log.e(LOG_TAG, "Result QR!!!!!!!!!!!!!!!!!: "+ mDecodeResult.symName);
+           // Log.e(LOG_TAG, "Result QR!!!!!!!!!!!!!!!!!: "+ mDecodeResult.decodeValue);
+            //String s = new String("Polish");
+            result = new String(mDecodeResult.decodeValue);
+            scannerInfoUpdate(native_ptr_, true);
+        }
+    }
+
+    //! Called from C++ to notify us that the associated C++ object is being destroyed.
+    public void cppDestroyed()
+    {
+        native_ptr_ = 0;
+    }
+
+	public synchronized boolean init() throws RemoteException {
+        try
+        {
+            Log.e(LOG_TAG, "Try Init scanner  ");
+            iScanner = new ScanManager();
+            if (iScanner != null) {
+                mDecodeResult = new DecodeResult();
+                Log.e(LOG_TAG, "Init scanner: MP60");
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+                iScanner.aDecodeSetResultType(ScanConst.ResultType.DCD_RESULT_USERMSG);
+                return true;
+	        }
+        }    
+        catch (final Throwable e)
+        {
+            Log.e(LOG_TAG, "Exception while starting Wso2GPSLocationListiner: ", e);
+            return false;
+        }
+        return false;
+	}
+    private native void scannerInfoUpdate(long native_ptr, boolean code);
+    private native Context getContext();
+
+} // class Wso2GPSLocationListiner
