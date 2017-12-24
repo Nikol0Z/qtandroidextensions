a7c0e91a3011e4779180801b484726e34e6c1a17
Uladzislau Vasilyeu <vasvlad@gmail.com>
2017-12-24 15:43:23 +0300

    Testing

--- QtAndroidHelpers/Mobility/QAndroidWso2gpslocationDataProvider.h
+++ QtAndroidHelpers/Mobility/QAndroidWso2gpslocationDataProvider.h
@@ -54,10 +54,11 @@ public:
 
 private:
 	friend void JNICALL Java_Wso2gpslocationListener_scannerInfoUpdate(JNIEnv *, jobject, jlong native_ptr, jboolean code);
-	friend void JNICALL Java_ru_dublgis_androidhelpers_mobility_Wso2gpslocationListener_scannerInfoUpdate(JNIEnv *, jobject, jlong native_ptr, jboolean code);
 
 public slots:
 	bool init();
+	void start();
+	void stop();
     QString result();
     double  lat();
     double  lon();
